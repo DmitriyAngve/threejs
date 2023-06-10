f7c795222f7e91e894c9177e059c0d6d437ac455
Dmitriy Angve <kirirch129@gmail.com>
2023-06-10 18:30:27 +0300

    #

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,28 @@
-// import React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+  // "AnimatePresence" компонент framer-motion, который используется для появления анимации в React-приложениях. Выступает как обёртка объектов, которые должны быть анимированы при добавлении и удалении и DOM.
+  <AnimatePresence>
+    {/* If we are on the the snap.intro in that case we can render the home page data */}
+    {snap.intro && (
+      // motion.div применяется к div, позволяет определить различные свойства анимации (initial, animate, exit)
+      <motion.section
+        className="home"
+        {...slideAnimation("left")}
+      ></motion.section>
+    )}
+  </AnimatePresence>;
 };
 
 export default Home;
---
+++ client/src/store/index.js
@@ -0,0 +1,16 @@
+import { proxy } from "valtio";
+
+// Set-up our state
+const state = proxy({
+  // intro meaning are we currently on the Home page, or are we not
+  intro: true,
+  // shirt color
+  color: "#EFBD480",
+  // logo shirt
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: "./threejs.png",
+  fullDecalL: "./threejs.png",
+});
+
+export default state;
